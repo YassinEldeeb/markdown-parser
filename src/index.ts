e886fd659c35a1f4dd57b46dca73f495287c0b6d
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-31 00:31:17 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,148 @@
+import fs from 'fs'
+import path from 'path'
+import { performance, PerformanceObserver } from 'perf_hooks'
+
+const perfObserver = new PerformanceObserver((items) => {
+  items.getEntries().forEach((entry) => {
+    console.log(entry)
+  })
+})
+
+perfObserver.observe({ entryTypes: ['measure'], buffered: true })
+
+performance.mark('start')
+
+// Rules
+const headings = {
+  '#': '1',
+  '##': '2',
+  '###': '3',
+  '####': '4',
+  '#####': '5',
+  '######': '6',
+}
+
+// Utils
+const allSameChar = (splittedLine: string[], char: string) =>
+  splittedLine.every((e) => e === char || e === '') && splittedLine.length > 0
+
+const formatText = (text: string) => {
+  const boldRegex = new RegExp('\\*\\*', 'g')
+  const italicRegex = new RegExp('\\*', 'g')
+  const bothRegex = new RegExp('\\*\\*\\*', 'g')
+
+  return text
+    .replace(bothRegex, '<b><i>')
+    .replace(boldRegex, '<b>')
+    .replace(italicRegex, '<i>')
+}
+
+const pureMD = fs
+  .readFileSync(path.join(__dirname, './sample.md'), 'utf-8')
+  .replace(/\r/g, '')
+
+const parsed: { type: string; [key: string]: string }[] = []
+
+const linesMD = pureMD.split('\n')
+const blackListedLinesIndexes: number[] = []
+
+linesMD.forEach((e, i) => {
+  const line = e.trim()
+
+  const splittedLine = line.split('')
+  if (splittedLine.length === 0) return
+
+  // Headings (Special Cases)
+  const nextLine = linesMD[i + 1]
+  const previousLine = linesMD[i - 1]
+
+  const nextSplittedLine = nextLine?.split('')
+
+  if (nextLine) {
+    if (allSameChar(nextSplittedLine, '=') && splittedLine.length > 0) {
+      parsed.push({
+        text: formatText(line),
+        type: 'h1',
+      })
+      blackListedLinesIndexes.push(i + 1)
+
+      return
+    } else if (
+      allSameChar(nextSplittedLine, '-') &&
+      splittedLine.length > 0 &&
+      !blackListedLinesIndexes.includes(i)
+    ) {
+      parsed.push({
+        text: formatText(line),
+        type: 'h2',
+      })
+      blackListedLinesIndexes.push(i + 1)
+
+      return
+    }
+  }
+
+  // Headings
+  let headingFound = false
+  Object.keys(headings).forEach((e) => {
+    const symbol = `${e} `
+    if (line.startsWith(symbol)) {
+      parsed.push({
+        text: formatText(line.replace(symbol, '')),
+        type: 'h' + headings[e as keyof typeof headings],
+      })
+      headingFound = true
+      return
+    }
+  })
+  if (headingFound) {
+    return
+  }
+
+  // Separator Line
+  if (
+    allSameChar(splittedLine, '-') &&
+    splittedLine.length >= 3 &&
+    !blackListedLinesIndexes.includes(i)
+  ) {
+    parsed.push({
+      type: 'separator',
+    })
+    return
+  }
+
+  // Paragraph
+  if (!blackListedLinesIndexes.includes(i)) {
+    // Append paragraph to the previous object If:
+    // 1.the last object in parsed is {type: 'p'}
+    // 2.there's a previous line
+    // 3.the previous line doesn't end with 2 or more spaces
+    const previousObj = parsed[parsed.length - 1]
+
+    if (
+      previousObj &&
+      previousObj.type === 'p' &&
+      previousLine.length > 0 &&
+      !previousLine.endsWith('  ')
+    ) {
+      previousObj.text = `${previousObj.text} ${line}`
+    } else {
+      parsed.push({
+        text: formatText(line),
+        type: 'p',
+      })
+    }
+  }
+})
+
+// Checking Performance
+performance.mark('end')
+performance.measure('performance', 'start', 'end')
+
+console.log(parsed)
+
+// Writing Output
+fs.writeFileSync(
+  path.join(__dirname, '../output.json'),
+  JSON.stringify(parsed, null, 2)
+)
---
+++ src/sample.md
@@ -0,0 +1,28 @@
+
+*S*pecial Big **1**
+===
+S***pe***cial Big 2
+----
+
+---
+
+This is the first line.  
+And this is the second line. 
+
+I think I'll use it to format all of my documents from now on. 
+
+I shouldn't go 
+on another 
+line
+
+ # Heading level 1
+
+## Heading level 2
+
+### Heading level 3
+
+#### Heading level 4
+
+##### Heading level 5
+
+###### Heading level 6
